4c8a6bcf88b718b5e100e6aa7f4df182f61fd8c2
xwdg <33448599+xwdg@users.noreply.github.com>
2018-03-14 19:35:08 +0800

    Add files via upload

---
+++ AddThingWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Work
+{
+    /// <summary>
+    /// AddThingWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class AddThingWindow : Window
+    {
+        public AddThingWindow()
+        {
+            InitializeComponent();
+            btnQr.IsEnabled = false;
+        }
+
+        private void btnQr_Click(object sender, RoutedEventArgs e)
+        {
+            //Mysql ms = new Mysql("work_319", "zhq", "z", "39.106.61.96");
+            //ms.add
+        }
+
+        private void btnQx_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (name.Text != "" && nums.Text != "" && price.Text != "")
+                btnQr.IsEnabled = true;
+            else
+                btnQr.IsEnabled = false;
+        }
+    }
+}
